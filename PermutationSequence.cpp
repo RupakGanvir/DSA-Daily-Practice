b2d642e6c1e05cac8aa8fd7822aca96162ca13ec
Rupak Ganvir <rupakganvir12@gmail.com>
2026-04-01 22:56:28 +0530

    Solution of Permutation Sequence Update

--- PermutationSequence.cpp
+++ PermutationSequence.cpp
@@ -12,7 +12,6 @@ public:
         int fact = 1;
         vector<int> numbers;
 
-        // 1. Precompute (n-1)! and fill numbers list {1, 2, ..., n}
         for (int i = 1; i < n; i++)
         {
             fact *= i;
@@ -21,20 +20,17 @@ public:
         numbers.push_back(n);
 
         string res = "";
-        k = k - 1; // Use 0-based indexing for easier math
+        k = k - 1;
 
         while (true)
         {
-            // 2. Find the index of the current digit
             res += to_string(numbers[k / fact]);
 
-            // 3. Remove that digit from the available pool
             numbers.erase(numbers.begin() + k / fact);
 
             if (numbers.empty())
                 break;
 
-            // 4. Update k and the factorial for the next position
             k %= fact;
             fact /= numbers.size();
         }
@@ -48,9 +44,6 @@ int main()
     Solution sol;
     int n = 4, k = 9;
 
-    // For n=4, k=9, the permutations are:
-    // 1234, 1243, 1324, 1342, 1423, 1432 (1-6)
-    // 2134, 2143, 2314... (7-9) -> 9th is 2314
     cout << "The " << k << "th permutation of " << n << " is: " << sol.getPermutation(n, k) << endl;
 
     return 0;
