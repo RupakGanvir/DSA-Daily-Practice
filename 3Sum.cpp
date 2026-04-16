27acd5d54aabed0c9a216830a7bcc7a9fb53eb77
Rupak Ganvir <rupakganvir12@gmail.com>
2026-04-17 00:45:48 +0530

    Solution of 3Sum

---
+++ 3Sum.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (nums[i] > 0)
+                break;
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = sol.threeSum(nums);
+
+    cout << "Unique triplets that sum to 0:" << endl;
+    for (const auto &triplet : result)
+    {
+        cout << "[" << triplet[0] << ", " << triplet[1] << ", " << triplet[2] << "]" << endl;
+    }
+
+    return 0;
+}
