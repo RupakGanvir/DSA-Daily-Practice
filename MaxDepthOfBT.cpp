c1e28128284ce83d7c1313a21de6f9544cb72a90
Rupak Ganvir <rupakganvir12@gmail.com>
2026-06-25 00:59:53 +0530

    Solution of Maximum Depth of Binary Tree

---
+++ MaxDepthOfBT.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return max(leftDepth, rightDepth) + 1;
+    }
+};
+
+int main()
+{
+    Solution solver;
+
+    TreeNode *root1 = new TreeNode(3);
+    root1->left = new TreeNode(9);
+    root1->right = new TreeNode(20);
+    root1->right->left = new TreeNode(15);
+    root1->right->right = new TreeNode(7);
+
+    TreeNode *root2 = new TreeNode(1);
+    root2->right = new TreeNode(2);
+
+    cout << "--- Maximum Depth of Binary Tree Tests ---" << endl;
+    cout << "Test 1: " << solver.maxDepth(root1) << endl;
+    cout << "Test 2: " << solver.maxDepth(root2) << endl;
+
+    return 0;
+}
