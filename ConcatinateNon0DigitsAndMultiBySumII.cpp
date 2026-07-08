43e9f611c50024cfac6fe8ec6c9d8e6e9f0d181c
Rupak Ganvir <rupakganvir12@gmail.com>
2026-07-08 14:44:56 +0530

    Solution of Concatenate Non Zero Digits and Multiply by Sum II

---
+++ ConcatinateNon0DigitsAndMultiBySumII.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+    const int MOD = 1e9 + 7;
+
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>> &queries)
+    {
+        int n = s.length();
+
+        vector<int> sumD(n + 1, 0);
+        vector<int> cntN0(n + 1, 0);
+        vector<long long> p(n + 1, 0);
+        vector<long long> pow10(n + 1, 1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+
+            int d = s[i - 1] - '0';
+
+            sumD[i] = sumD[i - 1] + d;
+            cntN0[i] = cntN0[i - 1] + (d > 0 ? 1 : 0);
+
+            if (d > 0)
+            {
+                p[i] = (p[i - 1] * 10 + d) % MOD;
+            }
+            else
+            {
+                p[i] = p[i - 1];
+            }
+        }
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+
+        for (const auto &q : queries)
+        {
+            int l = q[0], r = q[1];
+
+            int n0 = cntN0[r + 1] - cntN0[l];
+            int sd = sumD[r + 1] - sumD[l];
+
+            long long x = (p[r + 1] - (p[l] * pow10[n0]) % MOD + MOD) % MOD;
+
+            ans.push_back((x * sd) % MOD);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution solver;
+
+    string s = "10203004";
+    vector<vector<int>> queries = {{0, 7}, {1, 3}, {4, 6}};
+
+    vector<int> results = solver.sumAndMultiply(s, queries);
+
+    cout << "Results: ";
+    for (int res : results)
+    {
+        cout << res << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
