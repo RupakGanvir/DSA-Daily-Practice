a0506c9f14bb8fb10647035e1a6c62abd91c9375
Rupak Ganvir <rupakganvir12@gmail.com>
2026-08-21 02:04:14 +0530

    Solution of Subarray Sum Equals K

---
+++ SubarraySumEqualK.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> prefix_counts;
+        // Base case: one way to have a sum of 0 (an empty subarray before index 0)
+        prefix_counts[0] = 1;
+
+        int current_sum = 0;
+        int count = 0;
+
+        for (int num : nums)
+        {
+            current_sum += num;
+
+            if (prefix_counts.find(current_sum - k) != prefix_counts.end())
+            {
+                count += prefix_counts[current_sum - k];
+            }
+
+            prefix_counts[current_sum]++;
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    Solution solver;
+
+    vector<int> nums1 = {1, 1, 1};
+    int k1 = 2;
+    cout << solver.subarraySum(nums1, k1) << "\n";
+
+    vector<int> nums2 = {1, 2, 3};
+    int k2 = 3;
+    cout << solver.subarraySum(nums2, k2) << "\n";
+
+    return 0;
+}
